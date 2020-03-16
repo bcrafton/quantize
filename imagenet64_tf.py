4be8df91cbff2414fdf54272f57ff00cd8645843
Brian Crafton <crafton.b@husky.neu.edu>
2020-03-15 23:49:04 -0400

    try to make this run without batchnorm

--- imagenet64_tf.py
+++ imagenet64_tf.py
@@ -8,10 +8,13 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, default=10)
-parser.add_argument('--batch_size', type=int, default=64)
-parser.add_argument('--lr', type=float, default=1e-2)
+parser.add_argument('--batch_size', type=int, default=50)
+parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--eps', type=float, default=1.)
+parser.add_argument('--noise', type=float, default=2.)
 parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--init', type=str, default="glorot_uniform")
+parser.add_argument('--name', type=str, default="imagenet_weights")
 args = parser.parse_args()
 
 if args.gpu >= 0:
@@ -143,11 +146,11 @@ def block(x, f1, f2, p, name):
     filters2 = tf.Variable(init_filters(size=[3,3,f2,f2], init='alexnet'), dtype=tf.float32, name=name+'_conv2')
 
     conv1 = tf.nn.conv2d(x, filters1, [1,1,1,1], 'SAME')
-    bn1   = batch_norm(conv1, f2, name+'_bn1')
+    # bn1   = batch_norm(conv1, f2, name+'_bn1')
     relu1 = tf.nn.relu(bn1)
 
     conv2 = tf.nn.conv2d(relu1, filters2, [1,1,1,1], 'SAME')
-    bn2   = batch_norm(conv2, f2, name+'_bn2')
+    # bn2   = batch_norm(conv2, f2, name+'_bn2')
     relu2 = tf.nn.relu(bn2)
 
     pool = tf.nn.avg_pool(relu2, ksize=[1,p,p,1], strides=[1,p,p,1], padding='SAME')
@@ -156,9 +159,7 @@ def block(x, f1, f2, p, name):
 
 ###############################################################
 
-dropout_rate = tf.placeholder(tf.float32, shape=())
-learning_rate = tf.placeholder(tf.float32, shape=())
-
+'''
 block1 = block(features, 3,  64,   2, 'block1')                                      # 64
 block2 = block(block1,   64,  128,  2, 'block2')                                     # 32
 block3 = block(block2,   128, 256,  2, 'block3')                                     # 16
@@ -171,14 +172,62 @@ flat   = tf.reshape(pool, [args.batch_size, 1024])
 mat1   = tf.Variable(init_matrix(size=(1024, 1000), init='alexnet'), dtype=tf.float32, name='fc1')
 bias1  = tf.Variable(np.zeros(shape=1000), dtype=tf.float32, name='fc1_bias')
 fc1    = tf.matmul(flat, mat1) + bias1
+'''
+
+m = model(layers=[
+conv_block(3,    64, 1, noise=args.noise),
+conv_block(64,   64, 2, noise=args.noise),
+
+conv_block(64,   128, 1, noise=args.noise),
+conv_block(128,  128, 2, noise=args.noise),
+
+conv_block(128,  256, 1, noise=args.noise),
+conv_block(256,  256, 2, noise=args.noise),
+
+conv_block(256,  512, 1, noise=args.noise),
+conv_block(512,  512, 2, noise=args.noise),
+
+conv_block(512,  1024, 1, noise=args.noise),
+conv_block(1024, 1024, 1, noise=args.noise),
+
+avg_pool(4, 4),
+dense_block(1024, 10, noise=args.noise)
+])
 
 ###############################################################
 
+learning_rate = tf.placeholder(tf.float32, shape=())
+scale = tf.placeholder(tf.float32, [len(m.layers)])
+model_train = m.train(x=x)
+model_collect = m.collect(x=x)
+model_predict = m.predict(x=x, scale=scale)
+
+###############################################################
+
+'''
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=fc1, labels=labels))
 correct = tf.equal(tf.argmax(fc1, axis=1), tf.argmax(labels, 1))
 total_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
 
 train = tf.train.AdamOptimizer(learning_rate=learning_rate, epsilon=args.eps).minimize(loss)
+'''
+
+loss_class = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=model_train))
+params = tf.trainable_variables()
+
+loss_l2 = []
+for p in params:
+    loss_l2.append(tf.nn.l2_loss(p))
+loss_l2 = tf.reduce_sum(loss_l2)
+
+beta = 0.001 
+loss = loss_class + beta * loss_l2
+
+###############################################################
+
+grads = tf.gradients(loss, params)
+grads_and_vars = zip(grads, params)
+train = tf.train.AdamOptimizer(learning_rate=args.lr, epsilon=args.eps).apply_gradients(grads_and_vars)
 
 ###############################################################
 
@@ -200,41 +249,28 @@ for ii in range(0, args.epochs):
 
     sess.run(train_iterator.initializer, feed_dict={filename: train_filenames})
 
-    train_total = 0.0
-    train_correct = 0.0
-    train_acc = 0.0
-
     start = time.time()
     for jj in range(0, len(train_filenames), args.batch_size):
 
-        [_total_correct, _] = sess.run([total_correct, train], feed_dict={handle: train_handle, learning_rate: args.lr})
-
-        train_total += args.batch_size
-        train_correct += _total_correct
-        train_acc = train_correct / train_total
-
+        sess.run([train], feed_dict={handle: train_handle, learning_rate: args.lr})
         if (jj % (100 * args.batch_size) == 0):
             img_per_sec = (jj + args.batch_size) / (time.time() - start)
-            p = "%d | train accuracy: %f | img/s: %f" % (jj, train_acc, img_per_sec)
+            p = "%d | img/s: %f" % (jj, img_per_sec)
             print (p)
 
     ##################################################################
 
     sess.run(val_iterator.initializer, feed_dict={filename: val_filenames})
 
-    val_total = 0.0
-    val_correct = 0.0
-    val_acc = 0.0
-
     for jj in range(0, len(val_filenames), args.batch_size):
 
-        [_total_correct] = sess.run([total_correct], feed_dict={handle: val_handle, learning_rate: 0.0})
+        np_model_collect = sess.run(model_collect, feed_dict={handle: val_handle, learning_rate: 0.0})
+        scales.append(np_model_collect)
+            
+    scales = np.ceil(np.average(scales, axis=0))
 
-        val_total += args.batch_size
-        val_correct += _total_correct
-        val_acc = val_correct / val_total
-
-        if (jj % (100 * args.batch_size) == 0):
-            p = "val accuracy: %f" % (val_acc)
-            print (p)
+    ##################################################################
+            
+            
+            
 
