123a8115422d0f26b5d96974bed2a1e5f62532ca
Brian Crafton <crafton.b@husky.neu.edu>
2020-04-11 13:48:19 -0400

    new branch for post training quant

--- misc/hist.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-weights = np.load('cifar10_weights.npy', allow_pickle=True).item()
-for ii in range(6):
-    x = np.reshape(weights[ii][0], -1)
-    plt.hist(x, bins=100)
-    plt.show()
