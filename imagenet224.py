0e6b0f0b564897bc0998a46642c81b2d4ccb5f37
Brian Crafton <crafton.b@husky.neu.edu>
2020-04-12 23:51:39 -0400

    alright do train and collect differ

--- imagenet224.py
+++ imagenet224.py
@@ -280,15 +280,15 @@ val_handle = sess.run(val_iterator.string_handle())
 
 ###############################################################
 
-for ii in range(0, 2):
+for ii in range(0, 1):
     print('epoch %d/%d' % (ii, args.epochs))
 
-    sess.run(train_iterator.initializer, feed_dict={filename: train_imgs, label: train_labs})
+    sess.run(val_iterator.initializer, feed_dict={filename: val_imgs, label: val_labs})
 
     total_correct = 0
     start = time.time()
-    for jj in range(0, 100000, args.batch_size):
-        [np_sum_correct, _] = sess.run([train_sum_correct, train], feed_dict={handle: train_handle, learning_rate: args.lr})
+    for jj in range(0, 50000, args.batch_size):
+        [np_sum_correct, _] = sess.run([train_sum_correct, train], feed_dict={handle: val_handle, learning_rate: args.lr})
         total_correct += np_sum_correct
         if (jj % (100 * args.batch_size) == 0):
             acc = total_correct / (jj + args.batch_size)
@@ -298,16 +298,16 @@ for ii in range(0, 2):
 
 ##################################################################
 
-sess.run(train_iterator.initializer, feed_dict={filename: train_imgs, label: train_labs})
+sess.run(val_iterator.initializer, feed_dict={filename: val_imgs, label: val_labs})
 
 # MAKE SURE THIS IS SET CORRECTLY!!!
-collect_examples = 500 * args.batch_size
+collect_examples = 1000 * args.batch_size
 
 scales = []
 total_correct = 0
 start = time.time()
 for jj in range(0, collect_examples, args.batch_size):
-    [np_sum_correct, np_model_collect] = sess.run([sum_correct, model_collect], feed_dict={handle: train_handle, learning_rate: 0.})
+    [np_sum_correct, np_model_collect] = sess.run([sum_correct, model_collect], feed_dict={handle: val_handle, learning_rate: 0.})
     total_correct += np_sum_correct
 
     if len(scales):
