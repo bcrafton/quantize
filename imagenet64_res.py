592e899a5811ac25a781fbc2a87e1e093a1d1e34
Brian Crafton <crafton.b@husky.neu.edu>
2020-04-13 14:26:29 -0400

    it works, but dosnt have the max pool fix

--- imagenet64_res.py
+++ imagenet64_res.py
@@ -23,12 +23,11 @@ from bc_utils.init_tensor import init_matrix
 
 from layers import *
 
-MEAN = [122.77093945, 116.74601272, 104.09373519]
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 
 ###############################################################
-
-x = tf.placeholder(tf.float32, [args.batch_size, 224, 224, 3])
-y = tf.placeholder(tf.float32, [args.batch_size, 1000])
 
 weights = np.load('resnet18.npy', allow_pickle=True).item()
 
@@ -61,30 +60,30 @@ dense_block(512, 1000, noise=None, weights=weights)
 # very helpful:
 # https://pytorch.org/hub/pytorch_vision_resnet/
 
-x_process = x
+# pytorch resnet 18:
+# https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+
+# pytorch quantization: 
+# https://pytorch.org/blog/introduction-to-quantization-on-pytorch/
+
 # x_process = tf.keras.applications.resnet50.preprocess_input(x_process)
 # x_process = x_process / tf.math.reduce_std(x_process)
-model_predict = tf.nn.softmax(m.train(x=x_process))
 
-predict = tf.argmax(model_predict, axis=1)
-correct = tf.equal(predict, tf.argmax(y, 1))
-sum_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
-
-##################################################################
-
-config = tf.ConfigProto(allow_soft_placement=True)
-config.gpu_options.allow_growth=True
-sess = tf.Session(config=config)
-sess.run(tf.global_variables_initializer())
+def evaluate(x, y):
+    model_predict = tf.nn.softmax(m.train(x))
+    print (model_predict)
+    predict = tf.argmax(model_predict, axis=1)
+    actual = tf.argmax(y, 1)
+    correct = tf.equal(predict, actual)
+    sum_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
+    return predict, actual, sum_correct
 
 ##################################################################
 
 dataset = np.load('val_dataset.npy', allow_pickle=True).item()
 xs, ys = dataset['x'], dataset['y']
 
-# xs = xs - np.mean(xs, axis=(0,1,2))
-# xs = xs / np.std(xs, axis=(0,1,2))
-xs = xs / np.max(xs, axis=(0,1,2))
+xs = xs / 255. 
 xs = xs - np.array([0.485, 0.456, 0.406])
 xs = xs / np.array([0.229, 0.224, 0.225])
 
@@ -92,8 +91,9 @@ total_correct = 0
 for jj in range(0, 1024, args.batch_size):
     s = jj
     e = jj + args.batch_size
-    [np_sum_correct] = sess.run([sum_correct], feed_dict={x: xs[s:e], y: ys[s:e]})
-    total_correct += np_sum_correct
+    y, yhat, correct = evaluate(xs[s:e].astype(np.float32), ys[s:e])
+    total_correct += correct
+    print (y, yhat)
 
 acc = total_correct / 1024
 print (acc)
